3e688fb09e1db40723b8cdbdc0fb6077e27c15f9
David Wise <github@darksidestory.com>
2023-05-01 19:44:58 -0400

    Added PathTo.ps1 - first pass

---
+++ General/PathTo.ps1
@@ -0,0 +1,39 @@
+﻿param(
+    [string] $fileToFind
+)
+
+
+
+
+if ($fileToFind -eq $null -or $fileToFind -eq "") {
+    write-error "A file name to find is required"
+    exit 1
+}
+
+
+$searchPaths = "$((Get-Location).Path);$($env:Path)".split(";") | where { [string]::IsNullOrEmpty($_) -eq $false } | Select -Unique
+
+$fileMask = $fileToFind
+#if ($fileMask.IndexOfAny("?*".ToCharArray()) -lt 0) {
+#    $fileMask = "$($fileMask)*.*"
+#}
+
+#Write-host $fileMask -ForegroundColor Green
+$matchFound = $false
+$searchPaths | % {
+    $curPath = $_
+    $matches = [System.IO.Directory]::GetFiles($_, $fileMask)
+
+    if ($matches -ne $null) {
+        $matches | % {
+            $curMatch = $_
+            $item = get-item $curMatch
+            Write-Output $item.fullname
+            $matchFound = $true
+        }
+    }
+}
+
+if ($matchFound -eq $false) {
+    Write-Host "No match found" -ForegroundColor Yellow
+}
